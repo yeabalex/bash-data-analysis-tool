e9abb68273c7fbd71a77992e3d891522f6a0b586
yeabsira-alemu <yeabalex18@gmail.com>
2024-05-04 02:44:03 -0700

    agorithm to find unique values

--- functions/list-unique-values.sh
+++ functions/list-unique-values.sh
@@ -11,7 +11,37 @@ display_rows() {
          options+=("${n}" "${lines}")
         ((n++))
     done < <(head -n 1 "$file" | tr "," "\n")
-    choice=$(whiptail --title "Get unique item" --menu "Choose catagory" 20 70 15 "${options[@]}" 3>&1 1>&2 2>&3)
+    export choice=$(whiptail --title "Get unique item" --menu "Choose catagory" 20 70 15 "${options[@]}" 3>&1 1>&2 2>&3)
 }
 
+get_unique_values() {
+    values=$(awk -F '[,;]' -v choice="$choice" 'NR > 1 {print $choice}' "$file" | sort -n)
+    n=0
+    temp=$(head -n 1 <<< "$values")
+    declare -a unique_values=()
+    while read -r value
+    do
+        if [ "$n" -eq 0 ]; then
+            ((n++))
+        else
+            if [ "$temp" != "$value" ] && [ "$n" -eq 1 ]; then
+                unique_values+=("$temp")
+                temp=$value
+                n=1
+            elif [ "$temp" != "$value" ]; then
+                n=1
+                temp="$value"
+            else
+                ((n++))
+            fi
+        fi
+    done <<< "$values"
+
+    echo "${unique_values[@]}"
+}
+
+
+
+
 display_rows
+get_unique_values
